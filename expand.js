22eef4765d5ec7985028d49ee09f07991b297b72
Ivan Kozik <ivan@ludios.org>
2023-01-03 12:57:59 +0000

    Make goodreads work

---
+++ expand.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name        expand-everything
+// @namespace   ludios
+// @match       https://www.goodreads.com/book/show/*
+// @grant       none
+// @version     0.1
+// @author      ludios
+// @description Click the "show more" links to expand all the text on a page
+// ==/UserScript==
+
+const loc = window.location.href;
+
+function queryElements(selector, callback) {
+  const elements = document.querySelectorAll(selector);
+  elements.forEach(element => callback(element));
+}
+
+// Observe some selectors and run a callback for each selected element.
+// Automatically uninstall the MutationObserver after `maxMutations` mutations.
+function observe(maxMutations, selectors, callback) {
+  // For elements present before MutationObserver
+  for (let selector of selectors) {
+    queryElements(selector, callback);
+  }
+
+  let mutations = 0;
+  const observer = new MutationObserver(() => {
+    console.log(`mutations: ${mutations}`);
+    for (let selector of selectors) {
+      queryElements(selector, callback);
+    }
+    mutations++;
+    if (mutations >= maxMutations) {
+      console.log(`disconnecting MutationObserver after ${mutations} mutations`);
+      observer.disconnect();
+    }
+  });
+
+  observer.observe(document.documentElement, {
+    //attributes: true,
+    childList: true,
+    //characterData: true,
+    subtree: true,
+  });
+}
+
+
+if (loc.startsWith("https://www.goodreads.com/book/show/")) {
+  observe(100, ['a[data-text-id][onclick^="swapContent("]'], el => {
+  if (el.innerText == "...more") {
+    el.click();
+  }
+  });
+}
