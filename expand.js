9eb92c34288d331d399b34173c18491e09de0dca
Ivan Kozik <ivan@ludios.org>
2023-01-03 14:18:46 +0000

    Make YouTube work

--- expand.js
+++ expand.js
@@ -3,6 +3,7 @@
 // @namespace   ludios
 // @match       https://www.goodreads.com/book/show/*
 // @match       https://www.imdb.com/title/*/reviews*
+// @match       https://www.youtube.com/watch*
 // @grant       none
 // @version     0.1
 // @author      ludios
@@ -17,23 +18,37 @@ function queryElements(selector, callback) {
 }
 
 // Observe some selectors and run a callback for each selected element.
-function observe(selectors, callback) {
+function observe(maxMutations, selectors, callback) {
   // For elements present before MutationObserver
   for (let selector of selectors) {
     queryElements(selector, callback);
   }
 
+  let mutations = 0;
   const observer = new MutationObserver(() => {
+    mutations++;
+    if (mutations >= maxMutations) {
+      console.log(`disconnecting MutationObserver after ${mutations} mutations to avoid slowing down the page`);
+      observer.disconnect();
+    }
     for (let selector of selectors) {
       queryElements(selector, callback);
     }
   });
 
-  observer.observe(document.documentElement, {
-    //attributes: true,
-    childList: true,
-    //characterData: true,
-    subtree: true,
+  function reobserve() {
+    observer.observe(document.documentElement, {
+      childList: true,
+      subtree: true,
+    });
+  }
+
+  reobserve();
+
+  navigation.addEventListener('navigate', ev => {
+    console.log('navigated, resetting MutationObserver');
+    mutations = 0;
+    reobserve();
   });
 }
 
@@ -50,7 +65,7 @@ function clickIfUnclicked(el) {
 // Test page: https://www.goodreads.com/book/show/931984.The_Presentation_of_Self_in_Everyday_Life
 // Expected: all the lengthy user book reviews are expanded
 if (loc.startsWith("https://www.goodreads.com/book/show/")) {
-  observe(['a[data-text-id][onclick^="swapContent("]'], el => {
+  observe(100000, ['a[data-text-id][onclick^="swapContent("]'], el => {
     if (el.innerText == "...more") {
       el.click();
     }
@@ -65,9 +80,20 @@ if (loc.startsWith("https://www.goodreads.com/book/show/")) {
 // https://www.imdb.com/title/tt6710474/reviews?ref_=tt_urv
 // because there might be far too many reviews.
 if (loc.startsWith("https://www.imdb.com/title/")) {
-  observe(['.ipl-expander:not(.ipl-expander--expanded) > div > div'], el => {
+  observe(100, ['.ipl-expander:not(.ipl-expander--expanded) > div > div'], el => {
     // Avoid MutationObserver loop: imdb adds .ipl-expander--expanded
     // to the element some time _after_ you click.
     clickIfUnclicked(el);
+  });
+}
+
+if (loc.startsWith("https://www.youtube.com/")) {
+  observe(100, [
+    // Video description "show more"
+    '.button.ytd-text-inline-expander#expand',
+    // Video comments "show more" and replies
+    '.more-button',
+  ], el => {
+    el.click();
   });
 }
