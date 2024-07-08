2378ba143c9c89d31d5265c899c92fed832e5dfc
Ivan Kozik <ivan@ludios.org>
2024-07-08 17:31:38 +0000

    2.8.8

--- expand.user.js
+++ expand.user.js
@@ -5,7 +5,7 @@
 // @author      ludios
 // @license     AGPL-3.0; https://www.gnu.org/licenses/agpl-3.0.en.html
 // @grant       none
-// @version     2.8.7
+// @version     2.8.8
 //
 // @match       https://www.goodreads.com/book/show/*
 // @match       https://www.imdb.com/title/*/reviews*
